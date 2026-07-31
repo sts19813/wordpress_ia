cde1e66e67b819e93d03486370909bbb9c1f9393
sts19813 <sts19813@gmail.com>
2026-07-31 13:47:55 -0600

    use Mexico City application timezone

--- config/app.php
+++ config/app.php
@@ -61,11 +61,12 @@ return [
     |
     | Here you may specify the default timezone for your application, which
     | will be used by the PHP date and date-time functions. The timezone
-    | is set to "UTC" by default as it is suitable for most use cases.
+    | is set to Mexico City so application dates are displayed and scheduled
+    | consistently in Mexico in every environment.
     |
     */
 
-    'timezone' => 'UTC',
+    'timezone' => 'America/Mexico_City',
 
     /*
     |--------------------------------------------------------------------------
